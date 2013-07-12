657c40bb136b45de9e9dc661ec46993396b140d6
Chip Bell <chipbell4@gmail.com>
2013-07-11 23:59:29 -0400

    Rendering a little sphere now!

--- js/init.js
+++ js/init.js
@@ -33,4 +33,40 @@ function init() {
 	// attach the render-supplied DOM element
 	var body = document.getElementsByTagName('body')[0];
 	body.appendChild(renderer.domElement);
+
+	buildScene(scene);
+
+	renderer.render(scene, camera);
+}
+
+function buildScene(scene) {
+	// set up the sphere vars
+	var radius = 50,
+		 segments = 16,
+		 rings = 16;
+
+	var sphereMaterial =
+	  new THREE.MeshLambertMaterial({color: 0xCC0000});
+
+	var sphere = new THREE.Mesh(
+			new THREE.SphereGeometry(
+				radius,
+				segments,
+				rings),
+			sphereMaterial);
+
+	// add the sphere to the scene
+	scene.add(sphere);
+
+	// create a point light
+	var pointLight =
+	  new THREE.PointLight(0xFFFFFF);
+
+  // set its position
+  pointLight.position.x = 10;
+  pointLight.position.y = 50;
+  pointLight.position.z = 130;
+
+  // add to the scene
+  scene.add(pointLight);
 }
