966916b79b3b7cfdc6efea2209e287720ffaec71
Chip Bell <cbell@pbs.org>
2016-02-16 00:35:25 -0500

    Adding some keyboard state management

---
+++ src/KeyboardState.js
@@ -0,0 +1,43 @@
+importantKeys = {
+  38: 'up',
+  40: 'down',
+  37: 'left',
+  39: 'right',
+};
+
+module.exports = {
+  init: function() {
+    document.addEventListener('keydown', this.onKeyDown.bind(this));
+    document.addEventListener('keyup', this.onKeyUp.bind(this));
+
+    // initialize each key to false
+    var setFalseKey = function(keyCode) {
+      var direction = importantKeys[keyCode];
+      this[direction] = false;
+    };
+
+    Object.keys(importantKeys).forEach(setFalseKey.bind(this));
+  },
+
+  onKeyDown: function(event) {
+    var direction = importantKeys[event.keyCode];
+
+    if(direction === undefined) {
+      return;
+    }
+
+    this[direction] = true;
+
+    console.log(this);
+  },
+
+  onKeyUp: function(event) {
+    var direction = importantKeys[event.keyCode];
+
+    if(direction === undefined) {
+      return;
+    }
+
+    this[direction] = false;
+  },
+};
--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
+var KeyboardState = require('./KeyboardState');
 var GameManager = require('./GameManager');
 
+// Initialize
+KeyboardState.init();
 GameManager.init({ width: 800, height: 600 });
+
 GameManager.start();
