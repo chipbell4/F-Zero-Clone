d5bcda76f6bfe895cac80c7e5e83da0d6204566b
Chip Bell <cbell@pbs.org>
2016-06-22 01:03:48 -0400

    Using a turn direction variable

--- src/Car.js
+++ src/Car.js
@@ -1,14 +1,23 @@
 var PIXI = require('pixi.js');
 
+var textureForTurnDirection = function(direction) {
+  var textureName = 'forward.png';
+  if(direction !== 0) {
+    textureName = 'left' + Math.abs(direction) + '.png';
+  }
+  return PIXI.loader.resources['assets/falcon.json'].textures[textureName];
+};
+
 var Car = function() {
   this.isAlive = true;
-  var texture = PIXI.loader.resources['assets/falcon.json'].textures['forward.png'];
+  this.turnDirection = -1;
+  var texture = textureForTurnDirection(this.turnDirection);
   this.sprite = new PIXI.Sprite(texture);
   this.sprite.anchor.x = this.sprite.anchor.y = 0.5;
   this.sprite.position.x = this.sprite.position.y = 200;
 };
 
-Car.prototype.update = function() {
+Car.prototype.update = function(dt) {
 };
 
 module.exports = Car;
