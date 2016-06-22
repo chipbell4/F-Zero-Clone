220fbb0d9f85a3d489b685c752980c4377f84a47
Chip Bell <cbell@pbs.org>
2016-06-22 00:51:52 -0400

    Car sprite.

--- src/Car.js
+++ src/Car.js
@@ -2,9 +2,10 @@ var PIXI = require('pixi.js');
 
 var Car = function() {
   this.isAlive = true;
-  this.sprite = new PIXI.Sprite(PIXI.loader.resources['assets/falcon.json'].textures['forward.png']);
+  var texture = PIXI.loader.resources['assets/falcon.json'].textures['forward.png'];
+  this.sprite = new PIXI.Sprite(texture);
   this.sprite.anchor.x = this.sprite.anchor.y = 0.5;
-  this.sprite.position.x = this.sprite.position.y = 0;
+  this.sprite.position.x = this.sprite.position.y = 200;
 };
 
 Car.prototype.update = function() {
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var PIXI = require('pixi.js');
 var KeyboardState = require('./KeyboardState');
 var GameManager = require('./GameManager');
+var Car = require('./Car');
 
 PIXI.loader
   .add('assets/falcon.json')
@@ -8,6 +9,9 @@ PIXI.loader
     // Initialize
     KeyboardState.init();
     GameManager.init({ width: 800, height: 600 });
+
+    var car = new Car();
+    GameManager.addEntity(car);
 
     GameManager.start();
   });
