3f8cedb41fbadad9948768254a67cb8ae522cc75
Chip Bell <cbell@pbs.org>
2016-06-22 01:20:27 -0400

    Handling sprite updates for turns

--- src/Car.js
+++ src/Car.js
@@ -1,4 +1,7 @@
 var PIXI = require('pixi.js');
+var KeyboardState = require('./KeyboardState');
+
+var TURN_ANIMATION_SPEED = 0.25;
 
 var textureForTurnDirection = function(direction) {
   var textureName = 'forward.png';
@@ -8,6 +11,11 @@ var textureForTurnDirection = function(direction) {
   return PIXI.loader.resources['assets/falcon.json'].textures[textureName];
 };
 
+var scaleForTurnDirection = function(direction) {
+  // the sprite is left facing
+  return direction > 0 ? -1 : 1;
+}
+
 var Car = function() {
   this.isAlive = true;
   this.turnDirection = -1;
@@ -15,9 +23,30 @@ var Car = function() {
   this.sprite = new PIXI.Sprite(texture);
   this.sprite.anchor.x = this.sprite.anchor.y = 0.5;
   this.sprite.position.x = this.sprite.position.y = 200;
+
+  this.turnTime = 0;
 };
 
 Car.prototype.update = function(dt) {
+  this.turnTime += dt;
+  var noKeys = !KeyboardState.left && !KeyboardState.right;
+  var needsToTurn = this.turnTime > TURN_ANIMATION_SPEED;
+
+  if(needsToTurn && KeyboardState.right) {
+    this.turnTime = 0;
+    this.turnDirection += 1;
+    this.turnDirection = Math.min(3, this.turnDirection);
+  } else if(needsToTurn && KeyboardState.left) {
+    this.turnTime = 0;
+    this.turnDirection -= 1;
+    this.turnDirection = Math.max(-3, this.turnDirection);
+  } else if(needsToTurn && noKeys && this.turnDirection !== 0) {
+    this.turnTime = 0;
+    this.turnDirection -= this.turnDirection / Math.abs(this.turnDirection);
+  }
+
+  this.sprite.texture = textureForTurnDirection(this.turnDirection);
+  this.sprite.scale.x = scaleForTurnDirection(this.turnDirection);
 };
 
 module.exports = Car;
