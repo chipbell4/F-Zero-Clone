2c973086287f8356634ff3290d9c51919d801fd9
Chip Bell <cbell@pbs.org>
2016-06-22 00:32:43 -0400

    Dumb log...

--- src/Car.js
+++ src/Car.js
@@ -2,7 +2,9 @@ var PIXI = require('pixi.js');
 
 var Car = function() {
   this.isAlive = true;
-  this.sprite = new PIXI.Sprite(PIXI.resources['assets/falcon.json'].textures['forward.png']);
+  this.sprite = new PIXI.Sprite(PIXI.loader.resources['assets/falcon.json'].textures['forward.png']);
+  this.sprite.anchor.x = this.sprite.anchor.y = 0.5;
+  this.sprite.position.x = this.sprite.position.y = 0;
 };
 
 Car.prototype.update = function() {
